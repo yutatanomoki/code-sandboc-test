728ea42fe62393fe58bf57027da24c021ba5c89e
Yuta Tanomoki <74037339+yutatanomoki@users.noreply.github.com>
2022-03-07 07:27:47 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// アロー関数
+const func2 = (str) => str;
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
